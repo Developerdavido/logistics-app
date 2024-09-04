c2fe9002b9e8a11992a97c865a6319bef801a6f9
NonymousMorlock <agamspaul@gmail.com>
2024-09-04 23:02:40 +0000

    ✨ Feat: Create a SplashPage

---
+++ lib/src/splash/views/pages/splash_page.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:logistics_app/core/common/widgets/default_text.dart';
+import 'package:logistics_app/core/res/colours.dart';
+import 'package:logistics_app/src/onboarding/views/pages/onboarding_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  static const path = '/';
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacementNamed(OnboardingPage.path);
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: DefaultText(
+          'Dilivry',
+          fontSize: 32,
+          fontWeight: FontWeight.bold,
+          color: Colours.primary,
+        ),
+      ),
+    );
+  }
+}
