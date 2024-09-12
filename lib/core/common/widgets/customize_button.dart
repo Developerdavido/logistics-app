090724fa913f2d7497c56b5fd0b65c3832cce422
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2024-09-12 20:27:11 +0000

    Introduce reusable, customizable button component
    Created a versatile button component that can be easily customized to perform various actions, eliminating the need for repetitive code. This component allows for:
    
    - Customizable text, color, and styling
    - Support for different sizes and shapes
    - Consistent UI across the project

---
+++ lib/core/common/widgets/customize_button.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:logistics_app/core/res/colours.dart';
+
+class CustomizeButton extends StatelessWidget {
+  final String text;
+  final Color textColor;
+  final Color backgroundColor;
+  final Color borderColor;
+  final double borderRadius;
+  final VoidCallback onTap;
+  final double fontSize;
+  final EdgeInsets padding;
+  final double elevation;
+  final double? width;
+  final double? height;
+
+  const CustomizeButton({
+    super.key,
+    required this.text,
+    required this.onTap,
+    this.textColor = Colours.whiteColor,
+    this.backgroundColor = Colours.primary,
+    this.borderColor = Colors.transparent,
+    this.borderRadius = 8.0,
+    this.fontSize = 16.0,
+    this.padding = const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+    this.elevation = 2.0,
+    this.width,
+    this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        foregroundColor: textColor,
+        backgroundColor: backgroundColor,     // Text color
+        elevation: elevation,
+        padding: padding,
+        minimumSize: Size(width ?? 0, height ?? 0), // Setting min width and height
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(borderRadius),
+          side: BorderSide(color: borderColor),
+        ),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: fontSize,
+          fontWeight: FontWeight.w700,
+          color: textColor,
+        ),
+      ),
+    );
+  }
+}
