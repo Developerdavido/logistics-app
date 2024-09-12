6b67b152b9dad3287994349d215b9b9e59c348bf
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2024-09-12 20:00:05 +0000

    Register TripOverViewPage widget to routes
    Integrated the TripOverViewPage widget as the rider's home entry point. This enables riders to view, accept, or decline orders and proceed to the next stage in the workflow.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:logistics_app/core/res/colours.dart';
 import 'package:logistics_app/src/authentication/views/pages/otp_verification_page.dart';
+import 'package:logistics_app/src/home/views/pages/trip_overview_page.dart';
 import 'package:logistics_app/src/onboarding/views/pages/onboarding_page.dart';
 import 'package:logistics_app/src/splash/views/pages/splash_page.dart';
 
@@ -33,6 +34,7 @@ class App extends StatelessWidget {
         SplashPage.path: (_) => const SplashPage(),
         OnboardingPage.path: (_) => const OnboardingPage(),
         OtpVerificationPage.path: (_) => const OtpVerificationPage(),
+        TripOverviewPage.path: (_) => const TripOverviewPage(),
       },
     );
   }
