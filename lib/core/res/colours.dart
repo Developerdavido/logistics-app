21bb705cca8153beb2bb7e57d162cb3b65eff5c4
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2024-09-12 20:06:24 +0000

    Extend existing color palette
    This commit builds upon the color scheme initially created by [original author]. I've added additional colors to further enhance flexibility and consistency throughout the app. These changes make the palette more comprehensive while preserving the original structure, helping us avoid relying solely on Flutter's default colors.

--- lib/core/res/colours.dart
+++ lib/core/res/colours.dart
@@ -2,5 +2,11 @@ import 'dart:ui';
 
 abstract class Colours {
   static const primary = Color(0xFF925BFE);
-  static const primaryGradient = [Color(0xFF925BFE), Color(0xFFB6A2DE)];
+  static const primaryGradient = [Color(0xFF925BFE), Color(0xFFB6A2DE), Color(0xFFEAE0FF)];
+  static const whiteColor = Color(0xFFFFFFFF);
+  static const blackColor = Color(0xFF000000);
+  static const tripTypeColor=[Color(0xFFFEF0D3), Color(0xFFFFAA00)];
+  static const textColor=Color(0xFF909090);
+
+
 }
