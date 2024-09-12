f58ae4fe9f3d58b4fd4ae0640fd7b9db1aba5fa1
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2024-09-12 20:21:21 +0000

    Refactor Text with a String Utility Class
    applied OOP principles to consolidate and manage large amounts of text in the app. This approach reduces clutter in the project and streamlines access to text, making the code cleaner and more maintainable.

---
+++ lib/core/res/app_strings.dart
@@ -0,0 +1,27 @@
+class AppStrings{
+
+  //HomePage rider Strings
+  static const on='ON';
+  static const off='OFF';
+  static const home='Home';
+
+  // Orders information
+static const openOrder='Open Order';
+static const tripTypeText='Single Trip';
+static const userNameText='Wallace';
+static const roleText='Customer';
+
+static const pickUp='PickUp';
+static const addressForPickUp='No 2, Balonny Close, \nAllen';
+static const delivery='Delivery';
+static const addressForDelivery='87, south Lester Street, London Close \nBelgium';
+
+// Buttons Text
+  static const acceptOrder='Accept Order';
+  static const declineOrder='Decline Order';
+
+  // Buttons Text
+  static const sendingText='sending';
+  static const deliveryWeight='5kg';
+
+}
