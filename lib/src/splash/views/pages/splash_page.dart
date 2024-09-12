10c4481ef7db6f4a9f244670e522f41307d17243
NonymousMorlock <agamspaul@gmail.com>
2024-09-12 21:24:08 +0000

    ❌ Breaking Change: Change the splash screen logic.
    Previously it pushes after 3 seconds to the onboarding_page.dart, now it pushes after 2 seconds to either the onboarding_page.dart or the sign_in_page.dart if it is not the user's first time on the app

--- lib/src/splash/views/pages/splash_page.dart
+++ lib/src/splash/views/pages/splash_page.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:logistics_app/core/common/widgets/default_text.dart';
 import 'package:logistics_app/core/res/colours.dart';
+import 'package:logistics_app/core/utils/helpers/cache_helper.dart';
+import 'package:logistics_app/src/authentication/views/pages/sign_in_page.dart';
 import 'package:logistics_app/src/onboarding/views/pages/onboarding_page.dart';
 
 class SplashPage extends StatefulWidget {
@@ -20,8 +22,12 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    _timer = Timer(const Duration(seconds: 3), () {
-      Navigator.of(context).pushReplacementNamed(OnboardingPage.path);
+    _timer = Timer(const Duration(seconds: 2), () {
+      if (CacheHelper.instance.isFirstTimer) {
+        Navigator.of(context).pushReplacementNamed(OnboardingPage.path);
+      } else {
+        Navigator.of(context).pushReplacementNamed(SignInPage.path);
+      }
     });
   }
 
